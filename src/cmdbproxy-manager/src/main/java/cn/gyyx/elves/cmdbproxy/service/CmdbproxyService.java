c4300a170873260202bc1ca91cf46accd88f6c26
eastFu <492510931@qq.com>
2017-09-30 15:11:28 +0800

    cmdbproxy v0.0.1: code update

---
+++ src/cmdbproxy-manager/src/main/java/cn/gyyx/elves/cmdbproxy/service/CmdbproxyService.java
@@ -0,0 +1,10 @@
+package cn.gyyx.elves.cmdbproxy.service;
+
+/**
+ * @Author : east.Fu
+ * @Description :
+ * @Date : Created in  2017/9/30 15:09
+ */
+public interface CmdbproxyService {
+
+}
---
+++ src/cmdbproxy-manager/src/main/java/cn/gyyx/elves/cmdbproxy/service/impl/CmdbproxyServiceImpl.java
@@ -0,0 +1,13 @@
+package cn.gyyx.elves.cmdbproxy.service.impl;
+
+import cn.gyyx.elves.cmdbproxy.service.CmdbproxyService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author : east.Fu
+ * @Description :
+ * @Date : Created in  2017/9/30 15:09
+ */
+@Service("elvesConsumerService")
+public class CmdbproxyServiceImpl implements CmdbproxyService{
+}
