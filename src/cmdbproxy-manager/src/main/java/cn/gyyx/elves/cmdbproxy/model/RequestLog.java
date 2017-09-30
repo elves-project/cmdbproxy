538173aa82cb5b5b58fea20ee9aaf362472e9ed4
eastFu <492510931@qq.com>
2017-09-30 15:37:46 +0800

    cmdbproxy v0.0.1: code update

--- src/cmdbproxy-manager/src/main/java/cn/gyyx/elves/cmdbproxy/model/RequestLog.java
+++ src/cmdbproxy-manager/src/main/java/cn/gyyx/elves/cmdbproxy/model/RequestLog.java
@@ -1,9 +1,11 @@
 package cn.gyyx.elves.cmdbproxy.model;
 
+import java.io.Serializable;
+
 /**
  * @Author : east.Fu
  * @Description :
  * @Date : Created in  2017/9/30 15:32
  */
-public class RequestLog {
+public class RequestLog implements Serializable {
 }
--- src/cmdbproxy-manager/src/main/java/cn/gyyx/elves/cmdbproxy/service/impl/CmdbproxyServiceImpl.java
+++ src/cmdbproxy-manager/src/main/java/cn/gyyx/elves/cmdbproxy/service/impl/CmdbproxyServiceImpl.java
@@ -1,8 +1,11 @@
 package cn.gyyx.elves.cmdbproxy.service.impl;
 
+import cn.gyyx.elves.cmdbproxy.dao.CmdbproxyDao;
 import cn.gyyx.elves.cmdbproxy.service.CmdbproxyService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -13,12 +16,15 @@ import java.util.Map;
 @Service("elvesConsumerService")
 public class CmdbproxyServiceImpl implements CmdbproxyService{
 
+    @Autowired
+    private CmdbproxyDao cmdbproxyDao;
 
     @Override
     public Map<String, Object> getAgentInfoUril() {
+        Map<String,Object> data = new HashMap<String,Object>();
 
-
-        return null;
+        data.put("url","");
+        return data;
     }
 
 
