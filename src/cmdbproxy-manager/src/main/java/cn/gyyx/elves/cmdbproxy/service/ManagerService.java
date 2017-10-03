9717f6fa1f90207d39ab2685deaefff965ec13da
eastFu <492510931@qq.com>
2017-10-03 21:24:17 +0800

    cmdbproxy v0.0.1: code update

---
+++ src/cmdbproxy-manager/src/main/java/cn/gyyx/elves/cmdbproxy/service/ManagerService.java
@@ -0,0 +1,10 @@
+package cn.gyyx.elves.cmdbproxy.service;
+
+/**
+ * @Author : east.Fu
+ * @Description :
+ * @Date : Created in  2017/10/3 21:21
+ */
+public interface ManagerService {
+    
+}
---
+++ src/cmdbproxy-manager/src/main/java/cn/gyyx/elves/cmdbproxy/service/impl/ManagerServiceImpl.java
@@ -0,0 +1,11 @@
+package cn.gyyx.elves.cmdbproxy.service.impl;
+
+import cn.gyyx.elves.cmdbproxy.service.ManagerService;
+
+/**
+ * @Author : east.Fu
+ * @Description :
+ * @Date : Created in  2017/10/3 21:22
+ */
+public class ManagerServiceImpl implements ManagerService{
+}
