fd1de7d359c784550845162d27cc598092429774
eastFu <492510931@qq.com>
2017-10-04 21:31:17 +0800

    cmdbproxy v0.0.1: code update

---
+++ src/cmdbproxy-manager/src/main/java/cn/gyyx/elves/cmdbproxy/process/SecurityValidater.java
@@ -0,0 +1,10 @@
+package cn.gyyx.elves.cmdbproxy.process;
+
+import org.apache.log4j.Logger;
+
+public class SecurityValidater {
+
+    private static final Logger LOG = Logger.getLogger(SecurityValidater.class);
+
+
+}
